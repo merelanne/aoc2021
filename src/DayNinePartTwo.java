41c38f686224301da422fe7df369b911d8123255
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2022-06-25 16:14:24 +0200

    Add day nine part two solution

--- src/DayNinePartTwo.java
+++ src/DayNinePartTwo.java
@@ -7,33 +7,55 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DayNinePartTwo {
+    static List<char[]> covered;
+
     public static void main(String[] args) throws FileNotFoundException {
+        covered = new ArrayList<>();
         File f = new File("inputs/day-nine.txt");
         Scanner scan = new Scanner(f);
         List<String> data = new ArrayList<>();
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
             data.add(line);
+            covered.add(line.toCharArray());
         }
         int answer = findAnswer(data);
+        scan.close();
         System.out.println(answer);
     }
 
     public static int findAnswer(List<String> input) {
-        int answer = 0;
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < input.get(0).length(); j++) {
-                if (input.get(i).charAt(j) == '9') continue; // Still don't know why I need this
-                int current =  Character.digit(input.get(i).charAt(j), 10);
-                if (i > 0 && current > Character.digit(input.get(i - 1).charAt(j), 10)) continue;
-                if (i < input.size() - 1 && current > Character.digit(input.get(i + 1).charAt(j), 10)) continue;
-                if (j > 0 && current > Character.digit(input.get(i).charAt(j - 1), 10)) continue;
-                if (j < input.get(0).length() - 1 && current > Character.digit(input.get(i).charAt(j + 1), 10)) continue;
-                answer += current + 1;
-                System.out.println(current);
-                if (current == '9') System.out.println("I; " + i + ", j: " + j);
+        int[] basins = {0, 0, 0};
+        for (int x = 0; x < input.size(); x++) {
+            for (int y = 0; y < input.get(x).length(); y++) {
+                if (covered.get(x)[y] == '.') continue;
+                int size = basinSize(input, x, y);
+                int smallest = findMinIndex(basins);
+                if (size > basins[smallest]) basins[smallest] = size;
             }
         }
-        return answer;
+        return basins[0] * basins[1] * basins[2];
+    }
+
+    public static int basinSize(List<String> input, int x, int y) {
+        if (x < 0 || x >= input.size() || y < 0 || y >= input.get(x).length() || covered.get(x)[y] == '.') return 0;
+        char[] covered_x = covered.get(x);
+        covered_x[y] = '.';
+        covered.set(x, covered_x);
+
+        if (input.get(x).charAt(y) == '9') return 0;
+        return 1 + basinSize(input, x + 1, y) + basinSize(input, x, y + 1) + basinSize(input, x - 1, y) + basinSize(input, x, y - 1);        
+    }
+
+    public static int findMinIndex(int[] input) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int index = 0; index < input.length; index++) {
+            if (input[index] < min) {
+                min = input[index]; 
+                minIndex = index;
+            }
+        }
+        return minIndex;
     }
 }
